1ce40c0e054e52b590d0805bd1a73a8f12c0a11c
JuniorJPDJ <git@juniorjpdj.pl>
2025-11-21 00:02:00 +0100

    fix: remove promotions

--- promotions.py
+++ promotions.py
@@ -1,30 +1,7 @@
 """
 Promote other UNO bots
 """
-import random
-
-
-# Promotion messages and their weights
-PROMOTIONS = {
-    """
-For a more modern UNO experience, <a href="https://t.me/uno9bot/uno">try out</a> the new <a href="https://t.me/uno9bot?start=ref-unobot">@uno9bot</a>.
-""": 2.0,
-    """
-Also check out @UnoDemoBot, a newer version of this bot with exclusive modes and features!
-""": 1.0,
-}
-
-def get_promotion():
-    """ Get a random promotion message """
-    return random.choices(list(PROMOTIONS.keys()), weights=list(PROMOTIONS.values()))[0]
 
 async def send_promotion(chat, chance=1.0):
-    """ (Maybe) send a promotion message """
-
-    from utils import error
-    try:
-        if random.random() <= chance:
-            from shared_vars import application
-            await application.bot.send_message(chat.id, get_promotion(), parse_mode='HTML')
-    except Exception as e:
-        await error(None, None, e)
+    """ (Don't) send a promotion message """
+    pass
